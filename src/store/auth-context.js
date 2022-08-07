3bb3740b09d8086b7259e0e41d82be852870bf40
09eric09 <09eric09@gmail.com>
2022-08-07 12:21:37 -0700

    added onLogout as a default to Context API

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const AuthContext = React.createContext({
     //this is just an initial value
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {}
 });
 
 export default AuthContext;
